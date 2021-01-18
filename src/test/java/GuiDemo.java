acbe277ae1bf4a247d4894fb6d205e1078ecd05b
Daniel Gyorffy <judalion777@gmail.com>
2021-01-18 16:25:13 +0100

    Update Gui demo

--- src/test/java/GuiDemo.java
+++ src/test/java/GuiDemo.java
@@ -14,25 +14,36 @@ import java.util.function.Consumer;
 
 public class GuiDemo extends Application {
 
+    private static final String WINDOW_TITLE = "Dark Theme Detection";
+    private static final double WINDOW_WIDTH = 800;
+    private static final double WINDOW_HEIGHT = 600;
+
+    private static final String LIGHT_THEME_LABEL = "The OS uses LIGHT THEME";
+    private static final String DARK_THEME_LABEL = "The OS uses DARK THEME";
+    private static final double FONT_SIZE = 50;
+
+    private static final String DARK_STYLE = "-fx-base: #000000";
+    private static final String LIGHT_STYLE = "-fx-base: #d7d7d7";
+
     @Override
     public void start(Stage stage) {
         StringProperty labelValue = new SimpleStringProperty();
 
         stage.setScene(new Scene(buildRoot(labelValue)));
-        stage.setWidth(800);
-        stage.setHeight(600);
-        stage.setTitle("Dark theme detection");
+        stage.setWidth(WINDOW_WIDTH);
+        stage.setHeight(WINDOW_HEIGHT);
+        stage.setTitle(WINDOW_TITLE);
         stage.show();
 
         final OsThemeDetector detector = OsThemeDetector.getDetector();
         Consumer<Boolean> darkThemeListener = isDark -> {
             Platform.runLater(() -> {
                 if (isDark) {
-                    stage.getScene().getRoot().setStyle("-fx-base: #000000");
-                    labelValue.set("The OS uses DARK THEME");
+                    stage.getScene().getRoot().setStyle(DARK_STYLE);
+                    labelValue.set(DARK_THEME_LABEL);
                 } else {
-                    stage.getScene().getRoot().setStyle("-fx-base: #d7d7d7");
-                    labelValue.set("The OS uses LIGHT THEME");
+                    stage.getScene().getRoot().setStyle(LIGHT_STYLE);
+                    labelValue.set(LIGHT_THEME_LABEL);
                 }
             });
         };
@@ -43,7 +54,7 @@ public class GuiDemo extends Application {
     private Parent buildRoot(StringProperty labelValue) {
         Label label = new Label();
         label.textProperty().bind(labelValue);
-        label.setFont(Font.font(50));
+        label.setFont(Font.font(FONT_SIZE));
         return new StackPane(label);
     }
 
