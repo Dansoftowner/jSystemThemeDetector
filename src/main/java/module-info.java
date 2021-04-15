ee37e0ea92d07cb343be03d0756cb8800acddff4
Daniel Gyoerffy <49782833+Dansoftowner@users.noreply.github.com>
2021-04-15 12:08:30 +0200

    Adding modularity support

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.jthemedetector {
+    requires com.sun.jna.platform;
+    requires org.slf4j;
+    requires jfa;
+    requires org.jetbrains.annotations;
+    requires com.sun.jna;
+
+    exports com.jthemedetecor;
+}
